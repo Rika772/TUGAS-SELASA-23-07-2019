9d792cb6daec18ecf9ba6b6960fd4ce36f5c4061
Rika772 <53247373+Rika772@users.noreply.github.com>
2019-07-24 20:49:32 +0700

    Rika puspita

---
+++ php- java tugas selasa.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+ //Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+ 	public static void main(String args[])
+ 	{
+		int e;
+		int g;
+		int u;
+		
+		for(e=1; e<=5; e++)
+		{
+			
+ 		 for (g=5; g>e; g--)
+ 		 {
+			System.out.print("-");
+ 		 }
+		  for( u = 1 ; u <= e ; u++ ) 
+		  {
+ 		   System.out.print(" * " );
+		  }
+  	  	System.out.println("  ") ;
+
+		}		
+}
+}
+    
